44ba4bc6117a61d78ad688c93ebcb59d19062e77
obenxiaohai <805065973@qq.com>
2015-04-21 16:21:13 +0800

    1.0.3 修正了conrecordAll数组溢出的问题

--- VBServer/stuInfo/conRecordAll.js
+++ VBServer/stuInfo/conRecordAll.js
@@ -2,15 +2,22 @@ var fs = require('fs');
 var examPath = './exam/';
 //使用@#区分题间。使用#$区分题号与答案
 exports.WriteAnswer = function(paperID,ans){
+	console.log("用户提交全部题："+ans);
 	fs.readFile(examPath+paperID+'.json','utf8',function(err,data){
 		var json = JSON.parse(data);
 		var ansGroup = ans.split('@#');
 		var len = ansGroup.length;
 		ansGroup.forEach(function(Q_ans){
 			var Qnum = Q_ans.split('#$')[0];
+			console.log("Qnum:"+Qnum);
+			console.log("Q_ans:"+Q_ans);
 			var answer = Q_ans.split('#$')[1];
-			json[Qnum][2] = answer;
-			if(Qnum == 'Q'+len){
+			console.log("answer:"+answer);
+			console.log("json[Qnum]:"+json[Qnum]);
+			try{
+				json[Qnum][2] = answer;
+			}catch(e){}
+			if(Qnum == 'Q'+(len-1)){
 				fs.writeFile(examPath+paperID+'.json',JSON.stringify(json),'utf8',function(err){
 					if(err) throw err;
 				});
